99dd177e2cee7182db57536fa5524690110c2aa5
utam0k <k0ma@utam0k.jp>
2022-05-08 05:48:04 +0000

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Debug tool for [gitpodder](https://gitpod.io/)
+
+```console
+$ curl https://raw.githubusercontent.com/utam0k/ws-dbg/main/install.sh | bash
+```
+
+or
+
+```console
+$ wget https://github.com/utam0k/ws-dbg/releases/download/v0.0.2/wsdbg_0.0.2_linux_amd64.tar.gz
+$ tar -xzf wsdbg_0.0.2_linux_amd64.tar.gz
+```
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #/bin/bash
+
 set -eu
 
-VERSION=0.0.1
-wget https://github.com/utam0k/ws-dbg/releases/download/v${VERSION}/ws-dbg_${VERSION}_linux_amd64.tar.gz
-tar -xzf ws-dbg_${VERSION}_linux_amd64.tar.gz
-rm ws-dbg_${VERSION}_linux_amd64.tar.gz
+VERSION=0.0.2
+wget https://github.com/utam0k/ws-dbg/releases/download/v${VERSION}/wsdbg_${VERSION}_linux_amd64.tar.gz
+tar -xzf wsdbg_${VERSION}_linux_amd64.tar.gz
+rm wsdbg_${VERSION}_linux_amd64.tar.gz
